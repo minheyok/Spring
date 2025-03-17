95a36a09179139d46da87cdcf10e31650d33f3b3
unknown <wkdalsgur5556@gmail.com>
2025-03-17 12:26:44 +0900

    add ch07

---
+++ ch07/src/main/java/kr/co/ch07/dto/User1DTO.java
@@ -0,0 +1,28 @@
+package kr.co.ch07.dto;
+
+import kr.co.ch07.entity.User1;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User1DTO {
+    
+    private String uid;
+    private String name;
+    private String hp;
+    private String age;
+    public String email;
+    
+    //엔티티 변환 메서드 정의
+    public User1 toEntity(){
+        return User1.builder()
+                .uid(uid)
+                .name(name)
+                .hp(hp)
+                .age(age)
+                .build();
+    }
+
+}
